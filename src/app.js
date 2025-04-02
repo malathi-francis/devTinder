47f28a38664ce936cb23180bb6741de8ab7b5973
MalathiF <malathif@centizen.com>
2025-04-02 17:43:44 +0530

    basic routeMethod exploration- use,get,post (order)

--- src/app.js
+++ src/app.js
@@ -2,6 +2,13 @@ const express = require("express");
 
 const app = express();
 
+
+app.get('/me',(req,res) => {
+  res.send("get call check")
+});
+app.use('/me',(req,res) => {
+  res.send("use call check")
+});
 app.use('/app',(req,res) =>{
   res.send("data for this page shown here!")
 });
