60ee3822108fe9dadc00f051d0545f354e9dc3a9
MalathiF <malathif@centizen.com>
2025-04-07 17:41:12 +0530

    signup api(db- creation,connection)

--- src/app.js
+++ src/app.js
@@ -15,6 +15,17 @@ app.use('/app',(req,res) =>{
 app.use('/',(req,res) => {
   res.send("first page of the application");
 });
+
+//playing with the routes
+app.get('/ab?c',(req,res)=>{
+  res.send("b is optional in the route")
+});
+app.post('/ab+c',(req,res)=>{
+  res.send("b can be added infinitely")
+});
+app.post('./a(bc)+d',(req,res)=>{
+  res.send("bc together can be used infinitley")
+});
 app.listen(3000,() =>{
   console.log("server successfully connected to port 3000");
 });
